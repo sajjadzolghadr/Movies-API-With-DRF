058688e1044264b69322aca579d3597ef5bbfb88
sajjad <sajjadzolghadr777@gmail.com>
2026-07-14 13:05:14 +0330

    feat: add image upload support and media configuration

--- mysite/movies/models.py
+++ mysite/movies/models.py
@@ -6,5 +6,6 @@ class MovieData(models.Model):
     duration = models.FloatField()
     rating = models.FloatField()
     typ = models.CharField(max_length=200,default='action')
+    image = models.ImageField(upload_to='Images/',default='Images/None/Noimg.jpg')
     def __str__(self):
         return self.name
